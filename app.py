397f17b0278e49cec24fc9a3c2956bba24633da8
andrelganshitv@gmail.com <andrelganshitv@gmail.com>
2024-09-14 16:05:05 -0500

    añadir interfaz grafica

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 from module.similitudes import calcular_similitud_archivo
 
@@ -6,8 +6,8 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
-def hello_world():  
-    return "<p>Hello, World!</p>"
+def index():  
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
